7a48964fbd36f6b1e4d4f197bf4cfbb294f17392
wonderxxfull <emoskalenko97@gmail.com>
2023-03-12 12:46:56 +0200

    main.cs

---
+++ main.cs
@@ -0,0 +1,17 @@
+﻿using SilevenText.Cipher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilevenText
+{
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
